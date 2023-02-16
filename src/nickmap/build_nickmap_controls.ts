d4f03d74231a88ca8f005a0b9e6a200101acd16e
thehappycheese <the.nicholas.archer@gmail.com>
2023-02-16 06:34:51 +0000

    remove controls builder function

--- src/nickmap/build_nickmap_controls.ts
+++
@@ -1,7 +0,0 @@
-import { Control } from 'ol/control';
-
-export function build_nickmap_controls() {
-    let container_element = document.createElement("div");
-    container_element.setAttribute("id", "nickmap-controls-container");
-    return new Control({ element: container_element });
-}
